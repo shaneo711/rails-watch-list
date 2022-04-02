c033e7b9bf3d741023c8dec3a724498da37bf6a5
Shane Garland <shane.garland1@gmail.com>
2022-04-02 11:12:52 +1100

    created models

---
+++ db/migrate/20220402001044_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :overview
+      t.references :list, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
