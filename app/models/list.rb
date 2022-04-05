4f490069b265850c1e8718f6acee078af37899bf
Shane Garland <shane.garland1@gmail.com>
2022-04-05 19:23:55 +1000

    bundle fix

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
 
-  validates :name, uniqueness: true
-  validates :name, presence: true
+  validates :name, uniqueness: true, presence: true
 end
